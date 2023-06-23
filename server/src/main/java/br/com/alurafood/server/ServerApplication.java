b756f256d9ba1715dc405f2f1f05f52e966322b9
WendelEduardo <wendeleduardo2002@gmail.com>
2023-06-23 16:07:58 -0300

    Server da aplicação

---
+++ server/src/main/java/br/com/alurafood/server/ServerApplication.java
@@ -0,0 +1,15 @@
+package br.com.alurafood.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
